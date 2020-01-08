3577e4bfef377c351e7425a48425ca2c61a3ba94
Victor Johnson <johnsonsirv@gmail.com>
2020-01-08 13:45:15 +0100

    update production url from heroku

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ function App() {
   return (
     <div className="App">
       <h3>
-        Simple Calculator App [Mileston 1: Project Structure &amp; Live URL]
+        Simple Calculator App - Mileston 1: Project Structure &amp;&nbsp;
+        <a
+          href="https://mvp-react-calculator.herokuapp.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Live URL
+        </a>
       </h3>
     </div>
   );
