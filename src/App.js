b55f21592d17f2fa1010016d9304976b8114f681
Victor Johnson <johnsonsirv@gmail.com>
2020-01-08 13:07:47 +0100

    remove unnecessary files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>
+        Simple Calculator App [Mileston 1: Project Structure &amp; Live URL]
+      </h3>
+    </div>
+  );
+}
+
+export default App;
