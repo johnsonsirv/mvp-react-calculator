885dd2e1fab96011f62d281ce8ef9ddc31a6d3d3
Victor Johnson <johnsonsirv@gmail.com>
2020-01-12 00:47:50 +0100

    pass color, wide props to button

--- src/components/buttonPanel.jsx
+++ src/components/buttonPanel.jsx
@@ -12,32 +12,32 @@ class ButtonPanel extends Component {
     return (
       <div>
         <div className="row">
-          <Button name="AC" />
-          <Button name="+/-" />
-          <Button name="%" />
+          <Button name="AC" color="gray" />
+          <Button name="+/-" color="gray" />
+          <Button name="%" color="gray" />
           <Button name="÷" />
         </div>
         <div className="row">
-          <Button name="7" />
-          <Button name="8" />
-          <Button name="9" />
-          <Button name="X" />
+          <Button name="7" color="gray" />
+          <Button name="8" color="gray" />
+          <Button name="9" color="gray" />
+          <Button name="x" />
         </div>
         <div className="row">
-          <Button name="4" />
-          <Button name="5" />
-          <Button name="6" />
+          <Button name="4" color="gray" />
+          <Button name="5" color="gray" />
+          <Button name="6" color="gray" />
           <Button name="-" />
         </div>
         <div className="row">
-          <Button name="1" />
-          <Button name="2" />
-          <Button name="3" />
+          <Button name="1" color="gray" />
+          <Button name="2" color="gray" />
+          <Button name="3" color="gray" />
           <Button name="+" />
         </div>
         <div className="row">
-          <Button name="0" />
-          <Button name="." />
+          <Button name="0" wide color="gray" />
+          <Button name="." color="gray" />
           <Button name="=" />
         </div>
       </div>
